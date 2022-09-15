95d5f31b8a61258d908b690470aa137c10425085
dave1b <57140925+dave1b@users.noreply.github.com>
2022-09-15 10:34:15 +0200

    refactor

--- BlockProof.sol
+++ BlockProof.sol
@@ -1,66 +1,68 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract BlockProof {
     Authority[] authorities;
+    Software[] softwares;
 
     struct Authority {
         string name;
         uint256 authNumber;
         address owner;
-        Software[] softwares;
     }
 
     struct Software {
         string name;
         uint256 softwareNumber;
+        address owner;
         // mapping of softwareVersion to hashes
         mapping(string => string) hashes;
     }
 
-    function registerNewAuthority(string authorityName)
+    function registerNewAuthority(string memory authorityName)
         public
         returns (uint256 number)
     {
         uint256 authNumber = authorities.length;
-        Authority authority;
-        authority.name = authorityName;
-        authority.authNumber = authNumber;
-        authority.owner = msg.sender;
-        authorities.push(authority);
+        authorities.push(Authority(authorityName, authNumber, msg.sender));
         return authNumber;
     }
 
-    function registerNewSoftware(uint256 authNumber, string softwareName)
+    function registerNewSoftware(uint256 authNumber, string memory softwareName)
         public
         returns (uint256 number)
     {
-        uint256 newSoftwareNumber = authorities[authNumber].softwares.length;
-        authorities[authNumber].softwares.push(
-            Software(softwareName, newSoftwareNumber)
-        );
-        return newSoftwareNumber;
+        if (authorities[authNumber].owner != msg.sender) {
+            return 0;
+        } else {
+            uint256 newSoftwareNumber = softwares.length;
+            Software storage software = softwares.push();
+            software.name = softwareName;
+            software.softwareNumber = newSoftwareNumber;
+            software.owner = msg.sender;
+            return newSoftwareNumber;
+        }
     }
 
     function addNewSoftwareVersion(
-        uint256 authNumber,
         uint256 softwareNumber,
-        string softwareVersion,
-        string mewSoftwareHash
+        string memory softwareVersion,
+        string memory mewSoftwareHash
     ) public returns (bool sucessfull) {
-        authorities[authNumber].softwares[softwareNumber].hashes[
-                softwareVersion
-            ] = mewSoftwareHash;
-        return true;
+        if (softwares[softwareNumber].owner == msg.sender) {
+            softwares[softwareNumber].hashes[softwareVersion] = mewSoftwareHash;
+            return true;
+        }
+        return false;
     }
 
     function getSoftwareHash(
-        uint256 authNumber,
         uint256 softwareNumber,
-        string softwareVersion
-    ) public view returns (string hash) {
-        string storage _softwareHash = authorities[authNumber]
-            .softwares[softwareNumber]
-            .hashes[softwareVersion];
+        string memory softwareVersion
+    ) public view returns (string memory hash) {
+        string storage _softwareHash = softwares[softwareNumber].hashes[
+            softwareVersion
+        ];
         return _softwareHash;
     }
 }
